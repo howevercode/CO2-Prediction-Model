87636df9e4f3b59a4ce06c86c7eeabf474d649bb
Hari Anant <hari.anantchandola@gmail.com>
2025-04-01 07:40:48 +0530

    Add files via upload

---
+++ CO2 Prediction model.py
@@ -0,0 +1,16 @@
+import pandas as pd
+from sklearn import linear_model
+
+df = pd.read_csv("data.csv")
+
+print(df.head)
+
+x = df[['Weight', 'Volume']]
+y = df['CO2']
+
+regr = linear_model.LinearRegression()
+regr.fit(x, y)
+
+predictedCO2 = regr.predict([[1300, 800]])
+
+print(predictedCO2)
